f079bf0cfbf9a099bf1c33894541e55ca8a90bb7
Amit Das Gupta <amitdsgupta007@gmail.com>
2022-01-29 01:06:31 +0530

    Did socket saga code refactoring

---
+++ client/src/redux/helpers/sockets.js
@@ -0,0 +1,87 @@
+import { put, call, take, actionChannel, select } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
+import { getAllSocketsIds } from '../selectors/sockets';
+import * as types from '../constants/socket';
+
+//basic
+export const connect = function* (socket) {
+  yield put({ type: types.SOCKET_CONNECT_SUCCESS });
+  yield new Promise((resolve) => {
+    socket.on('connect', () => {
+      resolve(socket);
+    });
+  });
+};
+
+export const reconnect = (socket) => {
+  return new Promise((resolve) => {
+    socket.io.on('reconnect', () => {
+      resolve(socket);
+    });
+  });
+};
+
+export const disconnect = (socket) => {
+  return new Promise((resolve) => {
+    socket.on('disconnect', () => {
+      resolve(socket);
+    });
+  });
+};
+
+//sagas
+export const listenDisconnectSaga = function* (socketConnection) {
+  while (true) {
+    yield call(disconnect, socketConnection);
+    yield put({ type: types.SOCKET_DISCONNECT_SUCCESS });
+  }
+};
+
+export const listenConnectSaga = function* (socketConnection) {
+  while (true) {
+    yield call(reconnect, socketConnection);
+    yield put({ type: types.SOCKET_CONNECT_SUCCESS });
+  }
+};
+
+//channels
+const createLiveUserChannel = (socket) =>
+  eventChannel((emit) => {
+    const handler = (data) => {
+      emit(data);
+      console.log('user data', data);
+    };
+    socket.on('users', handler);
+    return () => {
+      socket.off('users', handler);
+    };
+  });
+
+export function* liveUserChannel(socketConnection) {
+  const liveUserChannel = yield call(createLiveUserChannel, socketConnection);
+  try {
+    while (true) {
+      const payload = yield take(liveUserChannel);
+      yield put({ type: types.SOCKET_LIVE_USER_FETCHED, payload });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function* followRequestActionChannel(socketConnection) {
+  const followRequestsChannel = yield actionChannel(
+    types.SOCKET_FOLLOW_REQUESTS_UPDATE
+  );
+
+  while (1) {
+    const { payload: { userId } = {} } = yield take(followRequestsChannel);
+    const allSocketIds = yield select(getAllSocketsIds);
+    const userFollowedSocketId = allSocketIds[userId];
+    if (userFollowedSocketId) {
+      socketConnection.emit('userFollowed', userFollowedSocketId);
+    } else {
+      //need to store it in backend db and show them this when he opens website
+    }
+  }
+}
--- client/src/redux/sagas/socketSaga.js
+++ client/src/redux/sagas/socketSaga.js
@@ -1,103 +1,15 @@
-import {
-  put,
-  call,
-  take,
-  race,
-  fork,
-  delay,
-  all,
-  actionChannel,
-  select,
-} from 'redux-saga/effects';
-import { eventChannel } from 'redux-saga';
+import { put, call, take, race, fork, delay, all } from 'redux-saga/effects';
 import * as types from '../constants/socket';
-import { getAllSocketsIds } from '../selectors/sockets';
+
+import {
+  connect,
+  listenDisconnectSaga,
+  listenConnectSaga,
+  liveUserChannel,
+  followRequestActionChannel,
+} from '../helpers/sockets';
 
 //helpers methods
-const connect = function* (socket) {
-  yield put({ type: types.SOCKET_CONNECT_SUCCESS });
-  yield new Promise((resolve) => {
-    socket.on('connect', () => {
-      resolve(socket);
-    });
-  });
-};
-
-const reconnect = (socket) => {
-  return new Promise((resolve) => {
-    socket.io.on('reconnect', () => {
-      resolve(socket);
-    });
-  });
-};
-
-const disconnect = (socket) => {
-  return new Promise((resolve) => {
-    socket.on('disconnect', () => {
-      resolve(socket);
-    });
-  });
-};
-
-const listenDisconnectSaga = function* (socketConnection) {
-  while (true) {
-    yield call(disconnect, socketConnection);
-    yield put({ type: types.SOCKET_DISCONNECT_SUCCESS });
-  }
-};
-
-const listenConnectSaga = function* (socketConnection) {
-  while (true) {
-    yield call(reconnect, socketConnection);
-    yield put({ type: types.SOCKET_CONNECT_SUCCESS });
-  }
-};
-
-// const getLiveUsers = function* (socketConnection) {
-//   const payload = yield call(listenLiveUsers, socketConnection);
-//   yield put({ type: types.SOCKET_LIVE_USER_FETCHED, payload });
-// };
-
-const createLiveUserChannel = (socket) =>
-  eventChannel((emit) => {
-    const handler = (data) => {
-      emit(data);
-      console.log('user data', data);
-    };
-    socket.on('users', handler);
-    return () => {
-      socket.off('users', handler);
-    };
-  });
-
-function* liveUserChannel(socketConnection) {
-  const liveUserChannel = yield call(createLiveUserChannel, socketConnection);
-  try {
-    while (true) {
-      const payload = yield take(liveUserChannel);
-      yield put({ type: types.SOCKET_LIVE_USER_FETCHED, payload });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function* followRequestActionChannel(socketConnection) {
-  const followRequestsChannel = yield actionChannel(
-    types.SOCKET_FOLLOW_REQUESTS_UPDATE
-  );
-
-  while (1) {
-    const { payload: { userId } = {} } = yield take(followRequestsChannel);
-    const allSocketIds = yield select(getAllSocketsIds);
-    const userFollowedSocketId = allSocketIds[userId];
-    if (userFollowedSocketId) {
-      socketConnection.emit('userFollowed', userFollowedSocketId);
-    } else {
-      //need to store it in backend db and show them this when he opens website
-    }
-  }
-}
 
 const listenDifferentChannels = function* (socketConnection) {
   yield all([
