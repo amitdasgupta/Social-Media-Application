9845b0c54bb42814f56008ad2e269e2c2802f31b
Amit Das Gupta <amitdsgupta007@gmail.com>
2022-02-28 12:46:20 +0530

    Added code to serve production build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const DB = require('./database/index');
 const rootController = require('./routes/index');
@@ -16,6 +17,9 @@ DbConnection.init();
 
 app.use(cors());
 app.use(express.json());
+if (process.env === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')));
+}
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan());
 app.use(morgan('common'));
@@ -23,6 +27,11 @@ app.use(morgan('common'));
 app.use('/', rootController);
 app.use(errorMiddleware);
 
+if (process.env === 'production') {
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
+}
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('backend server is running');
 });
