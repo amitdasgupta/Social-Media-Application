f4ccb5a1dd6919a26057426635a8bfb71f73e53b
Amit Das Gupta <amitdsgupta007@gmail.com>
2022-03-12 09:49:05 +0530

    Fixed coments UI design

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -108,6 +108,106 @@ export default function Comments() {
             <div className={styles.reactions}>{likes.length}</div>
           </div>
         </div>
+        <div className={styles.commentsItem}>
+          <div className={styles.commentsItemDetail}>
+            <div>
+              <Avatar
+                alt="Cindy Baker"
+                className={styles.profilePic}
+                src="https://storage.googleapis.com/download/storage/v1/b/social-connect-4d3e3.appspot.com/o/207d1f945c058d33c3ffeefdd82c547c.jpeg?generation=1646121249543202&alt=media"
+              />
+            </div>
+            <div>
+              Comment Text klkjiuh ijjjujh uuhhyuh khh jhdcjsdc cakocijaij
+              scjijcnhc cjuihhuc
+            </div>
+          </div>
+          <div className={styles.commentsItemEdit}>
+            <MoreVert />
+          </div>
+          <div className={styles.likeDislike}>
+            <ThumbUp
+              className={isPostLiked ? styles.thumbUp : styles.thumbDown}
+              // onClick={handleReaction}
+            />
+            <div className={styles.reactions}>{likes.length}</div>
+          </div>
+        </div>
+        <div className={styles.commentsItem}>
+          <div className={styles.commentsItemDetail}>
+            <div>
+              <Avatar
+                alt="Cindy Baker"
+                className={styles.profilePic}
+                src="https://storage.googleapis.com/download/storage/v1/b/social-connect-4d3e3.appspot.com/o/207d1f945c058d33c3ffeefdd82c547c.jpeg?generation=1646121249543202&alt=media"
+              />
+            </div>
+            <div>
+              Comment Text klkjiuh ijjjujh uuhhyuh khh jhdcjsdc cakocijaij
+              scjijcnhc cjuihhuc
+            </div>
+          </div>
+          <div className={styles.commentsItemEdit}>
+            <MoreVert />
+          </div>
+          <div className={styles.likeDislike}>
+            <ThumbUp
+              className={isPostLiked ? styles.thumbUp : styles.thumbDown}
+              // onClick={handleReaction}
+            />
+            <div className={styles.reactions}>{likes.length}</div>
+          </div>
+        </div>
+        <div className={styles.commentsItem}>
+          <div className={styles.commentsItemDetail}>
+            <div>
+              <Avatar
+                alt="Cindy Baker"
+                className={styles.profilePic}
+                src="https://storage.googleapis.com/download/storage/v1/b/social-connect-4d3e3.appspot.com/o/207d1f945c058d33c3ffeefdd82c547c.jpeg?generation=1646121249543202&alt=media"
+              />
+            </div>
+            <div>
+              Comment Text klkjiuh ijjjujh uuhhyuh khh jhdcjsdc cakocijaij
+              scjijcnhc cjuihhuc
+            </div>
+          </div>
+          <div className={styles.commentsItemEdit}>
+            <MoreVert />
+          </div>
+          <div className={styles.likeDislike}>
+            <ThumbUp
+              className={isPostLiked ? styles.thumbUp : styles.thumbDown}
+              // onClick={handleReaction}
+            />
+            <div className={styles.reactions}>{likes.length}</div>
+          </div>
+        </div>
+        <div className={styles.commentsItem}>
+          <div className={styles.commentsItemDetail}>
+            <div>
+              <Avatar
+                alt="Cindy Baker"
+                className={styles.profilePic}
+                src="https://storage.googleapis.com/download/storage/v1/b/social-connect-4d3e3.appspot.com/o/207d1f945c058d33c3ffeefdd82c547c.jpeg?generation=1646121249543202&alt=media"
+              />
+            </div>
+            <div>
+              Comment Text klkjiuh ijjjujh uuhhyuh khh jhdcjsdc cakocijaij
+              scjijcnhc cjuihhuc
+            </div>
+          </div>
+          <div className={styles.commentsItemEdit}>
+            <MoreVert />
+          </div>
+          <div className={styles.likeDislike}>
+            <ThumbUp
+              className={isPostLiked ? styles.thumbUp : styles.thumbDown}
+              // onClick={handleReaction}
+            />
+            <div className={styles.reactions}>{likes.length}</div>
+          </div>
+        </div>
       </div>
       <div className={styles.commentsInput}>
         <Avatar
